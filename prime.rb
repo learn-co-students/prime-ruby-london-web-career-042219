fe7471ada9b6f81fb56bdc96bd5f84ac8d4dcaa8
Marcus Mellor <marcus@mellor.com>
2019-03-17 18:24:29 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
 # Add  code here!
+def prime?( value )
+  test_array=(2...value).to_a
+  return_value=false
+  if value > 1
+    return_value=test_array.none?{|x| value%x==0}
+  end
+  return_value
+end
