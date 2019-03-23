9b24824738e335363464937b7a2571bbab33943d
rossmawd <rossmawd@gmail.com>
2019-03-23 17:42:03 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(num)
+  range = (2..num).to_a
+  return true if num == 2
+  return false if num <= 1
+  range.each{|x|
+    if x == range.last
+      return true    
+    elsif num == x
+      next
+    elsif num % x  == 0
+      return false
+    else
+      next
+    end
+    }
+end
