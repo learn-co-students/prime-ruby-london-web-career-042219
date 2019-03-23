1750a4facaa88a8836dc1cea4720fa3ffdfdc904
Daniel Bovington <d.bovington@gmail.com>
2019-03-23 18:17:00 +0000

    Done.

--- spec/prime_spec.rb
+++ spec/prime_spec.rb
@@ -1,6 +1,20 @@
 require_relative './spec_helper'
 require_relative '../prime.rb'
 
+def prime (numbers)
+  @nth_value = nth_value
+ for test_value in numbers do
+  if (@nth_value % test_value) == 0
+   puts ("#{@nth_value} is not divisible by #{numbers}")
+  else
+   puts ("This is not a prime number since this is divisible by #{test_value}")
+  break
+  end
+ end
+ end
+end
+
+
 describe "prime?" do
   it 'returns true for prime numbers' do
     expect(prime?(2)).to be(true)
